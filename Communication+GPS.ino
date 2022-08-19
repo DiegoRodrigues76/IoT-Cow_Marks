27ded9e35b0911d14a8d3da52ad7b2f3da5c2ae5
marialuizavas <84046707+marialuizavas@users.noreply.github.com>
2022-08-19 16:01:32 -0300

    Update Communication+GPS.ino

--- Communication+GPS.ino
+++ Communication+GPS.ino
@@ -3,21 +3,20 @@
 
 #include <RoboCore_SMW_SX1262M0.h>
 #include <SoftwareSerial.h>
-#include <TinyGPS.h>
 #define INTERVALO 5*60*106
-#define GPS_RX 4
-#define GPS_TX 3
-#define GPS_Serial_Baud 9600
-
 long timeStamp;
-TinyGPS gps;
 
 // --------------------------------------------------
 // Variables
 
 SoftwareSerial ss(10,11);
+SoftwareSerial SerialGPS(3, 4);
+TinyGPS GPS;
 SMW_SX1262M0 lorawan(ss);
-SoftwareSerial gpsSerial(GPS_RX, GPS_TX);
+
+float lat, lon, vel;
+unsigned long data, hora;
+unsigned short sat;
 
 // --------------------------------------------------
 // --------------------------------------------------
@@ -31,31 +30,17 @@ void setup() {
   ss.begin(9600);
   timeStamp = millis();
   
-  //GPS
-  Serial.begin(GPS_Serial_Baud);
-  gpsSerial.begin(GPS_Serial_Baud);
+  // GPS
+  SerialGPS.begin(9600);
+  Serial.begin(9600);
+ 
+  Serial.println("Buscando satelites...");
 }
 
 // --------------------------------------------------
 // --------------------------------------------------
 
-void loop() 
-{
-  // GPS
-  bool newData = false;
-  unsigned long chars;
-  
-  // For one second we parse GPS data and report some key values
-  for (unsigned long start = millis(); millis() - start < 1000;)
-  {
-    while (gpsSerial.available())
-    {
-      char c = gpsSerial.read();
-      // Serial.write(c); //apague o comentario para mostrar os dados crus
-      if (gps.encode(c)) // Atribui true para newData caso novos dados sejam recebidos
-        newData = true;
-    }
- 
+void loop() {
   // SMW_SX1262M0 to computer
   if(ss.available()){
     Serial.write(ss.read());
@@ -68,25 +53,62 @@ void loop()
   if ((millis() - timeStamp) > 3* (long)INTERVALO){
     ss.println("AT+SEND=10:TESTE");
     timeStamp = millis();
-  }
-}
+  } 
   
-  if (newData)
-  {
-    float flat, flon;
-    unsigned long age;
-    gps.f_get_position(&flat, &flon, &age);
-    Serial.print("LAT=");
-    Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
-    Serial.print(" LON=");
-    Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
-    Serial.print(" SAT=");
-    Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
-    Serial.print(" PREC=");
-    Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
-    Serial.println();
-    Serial.println();
-  }
+    while (SerialGPS.available()) {
+    if (GPS.encode(SerialGPS.read())) {
+ 
+      //Hora e data
+      GPS.get_datetime(&data, &hora);
+      
+      Serial.print("--");
+      Serial.print((hora / 1000000) - 3);
+      Serial.print(":");
+      Serial.print((hora % 1000000) / 10000);
+      Serial.print(":");
+      Serial.print((hora % 10000) / 100);
+      Serial.print(" -- ");
+ 
+      Serial.print(data / 10000);
+      Serial.print("/");
+      Serial.print((data % 10000) / 100);
+      Serial.print("/");
+      Serial.print(data % 100);
+      Serial.println("--");
+      
+      //latitude e longitude
+      GPS.f_get_position(&lat, &lon);
+ 
+      Serial.print("Latitude: ");
+      Serial.println(lat, 6);
+      Serial.print("Longitude: ");
+      Serial.println(lon, 6);
+ 
+      //velocidade
+      vel = GPS.f_speed_kmph();
+ 
+      Serial.print("Velocidade: ");
+      Serial.println(vel);
+
+      Serial.print("http://maps.google.com/maps?q=");
+      Serial.print(lat, 6);
+      Serial.print(",");
+      Serial.print(lon, 6);
+
+      Serial.println("");
+     
+ 
+      //Satelites
+      sat = GPS.satellites();
+      
+ 
+      if (sat != TinyGPS::GPS_INVALID_SATELLITES) {
+        Serial.print("Satelites: ");
+        Serial.println(sat);
+      }
+
+    Serial.println("");
+    
 }
 
 // --------------------------------------------------
